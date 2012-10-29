82a034f84eafe0d6f16d0cb104ff5c857af2325a
Stoyan Stoyanov <djstoxer@gmail.com>
2012-10-29 17:13:54 +0200

    added first player to the game. Now we can draw the player.

--- PingPongGame/Program.cs
+++ PingPongGame/Program.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace PingPongGame
 {
-    class Program
+
+    class Player
+    {
+        public int CoordXOfPad { get; set; }
+        public int LenghtOfPad { get; set; }
+
+        public Player(int coorXPad, int lenOfPad)
+        {
+            CoordXOfPad = coorXPad;
+            LenghtOfPad = lenOfPad;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(0, CoordXOfPad);
+            for (int i = 0; i < LenghtOfPad; i++)
+            {
+                Console.WriteLine('|');
+            }
+        }
+
+    }
+
+    class Game
     {
         static void Main(string[] args)
         {
             Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
 
-            Console.WriteLine("Hasmu pere budjandas!");
+            Player firstPlayer = new Player(10, 5);
+
+            while (true)
+            {
+                firstPlayer.Draw();
+
+                Thread.Sleep(60);
+            }
+            
         }
     }
 }
