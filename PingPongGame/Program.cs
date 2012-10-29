c73090df914ba8668bfbdbac841ae0255c5ecbf8
Stoyan Stoyanov <djstoxer@gmail.com>
2012-10-29 16:32:25 +0200

    First commit

---
+++ PingPongGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+        }
+    }
+}
